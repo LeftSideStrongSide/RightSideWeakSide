9e343ec3c728f44745e29a13475494cbaada488d
Zachary Hughes <zakk.hughes@gmail.com>
2015-08-10 14:12:36 -0500

    added useless php files because alissa liked her styling better

---
+++ views/partials/footer.php
@@ -0,0 +1,5 @@
+<div class="col-lg-12 text-center footer-below">
+	<em>Copyright 2015</em><br>
+	<em>Poorly coded testing by:</em><br>
+	<em>Zach & Alissa</em>
+</div>
---
+++ views/partials/header.php
@@ -0,0 +1,3 @@
+<header class="col-lg-12 text-center">
+	<h1 class="header text-center">Welcome to this website. This is the header</h1>
+</header>
