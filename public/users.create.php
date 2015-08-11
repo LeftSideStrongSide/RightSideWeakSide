e81c1411b85a89de7e6589e9e7b715fc845c632e
asteph <alissa.stephens1@gmail.com>
2015-08-11 10:39:24 -0500

    updated migration and seeder to allow for user emails and created login/ user set up pages

---
+++ public/users.create.php
@@ -0,0 +1,84 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="Create an account for Adlister.">
+
+	<title>New User</title>
+
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<style>
+	body {
+		padding-top: 40px;
+		padding-bottom: 40px;
+		background-color: #eee;
+	}
+
+	.form-signin {
+		max-width: 330px;
+		padding: 15px;
+		margin: 0 auto;
+	}
+	.form-signin .form-signin-heading,
+	.form-signin .checkbox {
+		margin-bottom: 10px;
+	}
+	.form-signin .checkbox {
+		font-weight: normal;
+	}
+	.form-signin .form-control {
+		position: relative;
+		height: auto;
+		-webkit-box-sizing: border-box;
+		-moz-box-sizing: border-box;
+		box-sizing: border-box;
+		padding: 10px;
+		font-size: 16px;
+	}
+	.form-signin .form-control:focus {
+		z-index: 2;
+	}
+	.form-signin input[type="email"] {
+		margin-bottom: -1px;
+		border-bottom-right-radius: 0;
+		border-bottom-left-radius: 0;
+	}
+	.form-signin input[type="password"] {
+		border-top-left-radius: 0;
+		border-top-right-radius: 0;
+	}
+	#confirmPassword{
+		margin-bottom: 20px;
+	}
+	</style>
+</head>
+
+<body>
+
+	<div class="container">
+
+		<form class="form-signin">
+			<h2 class="form-signin-heading">Create a New User</h2>
+			<label for="username" class="sr-only">Username</label>
+			<input type="text" id="username" class="form-control" placeholder="Username" name='username' required autofocus>
+			<label for="inputEmail" class="sr-only">Email address</label>
+			<input type="email" id="inputEmail" class="form-control" placeholder="Email address" name='email' required>
+			<label for="inputPassword" class="sr-only">Password</label>
+			<input type="password" id="inputPassword" class="form-control" placeholder="Password" name='password' required>
+			<label for="confirmPassword" class="sr-only">Confirm Password</label>
+			<input type="password" id="confirmPassword" class="form-control" placeholder="Confirm Password" name='confirmPassword' required>
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+		</form>
+
+	</div> <!-- /container -->
+
+</body>
+</html>
