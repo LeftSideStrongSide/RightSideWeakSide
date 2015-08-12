9e52ce9ce7b7b3f842f1baf5a1f7f2ec1e8950d7
asteph <alissa.stephens1@gmail.com>
2015-08-12 18:27:24 -0500

    had ads.create link to users.show if an ad was succesfully created, started users.show, and changed navbar to deal with ad related things

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -16,15 +16,15 @@
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
         <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse Ads<span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><a href="#">Furniture</a></li>
+            <li><a href="#">Appliances</a></li>
+            <li><a href="#">Entertainment</a></li>
             <li role="separator" class="divider"></li>
-            <li class="dropdown-header">Nav header</li>
-            <li><a href="#">Separated link</a></li>
-            <li><a href="#">One more separated link</a></li>
+            <li class="dropdown-header">All Ads</li>
+            <li><a href="#">Most Recent</a></li>
+            <li><a href="#">Free</a></li>
           </ul>
         </li>
       </ul>
