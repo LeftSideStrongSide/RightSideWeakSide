d6a688606527df2ee1232f7447ab769b2491dfee
Zachary Hughes <zakk.hughes@gmail.com>
2015-08-12 08:53:38 -0500

    linked log out to nav bar

--- public/auth.logout.php
+++ public/auth.logout.php
@@ -8,6 +8,8 @@
 		        );
 		    }
 		    session_destroy();
+		    //MAKES THE USER THINK ITS PROCESSING THEIR LOGOUT REQUEST	
+			sleep(1);
 		    header('Location: auth.login.php');
 		    exit();
 ?>
