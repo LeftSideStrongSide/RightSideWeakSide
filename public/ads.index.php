206e9ced7eac0446d0a8fa0c50a59fb397bbb16d
asteph <alissa.stephens1@gmail.com>
2015-08-13 15:22:45 -0500

    added prices to ads

--- public/ads.index.php
+++ public/ads.index.php
@@ -21,6 +21,7 @@ $allAds = Ads::all()->attributes;
       <img class="img-responsive img-thumbnail " src="<?= $ad['image_url'] ?>" alt="ad image">
       <h2><?= $ad['item_name']; ?></h2>
       <p><?= $ad['description']; ?></p>
+      <p>$<?= $ad['price']; ?></p>
       <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
     </div><!--/.col-xs-6.col-lg-4-->
   <?php endforeach;?>
