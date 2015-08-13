5ee7a02886c90eb85f65963187f5248c1965d9f9
asteph <alissa.stephens1@gmail.com>
2015-08-13 14:18:56 -0500

    took double links

--- public/users.show.php
+++ public/users.show.php
@@ -83,10 +83,9 @@
 				    <h2><?= $ad['item_name']; ?></h2>
 				    <p><?= $ad['description']; ?></p>
 				    <p>
-				    	<a id="" class="btn btn-default" href="#" role="button">Edit &raquo;</a>
+				    	<a id="" class="btn btn-default" href="ads.edit.php?id=<?=$ad['id']?>" role="button">Edit &raquo;</a>
 				    	<a class="btn btn-default" href="?ad=<?= $ad['id'] ?>" role="button">Delete &raquo;</a>
 				    </p>
-				    <p><a class="btn btn-default" href="ads.edit.php?id=<?=$ad['id']?>" role="button">Edit &raquo;</a><a class="btn btn-default" href="#" role="button">Delete &raquo;</a></p>
 
 				  </div><!--/.col-xs-6.col-lg-4-->
 				<?php endforeach; }?>
